e4e02d994dab11fb1d303c116342c21a9ecc6f84
Colin Cason <colinjcason@gmail.com>
2019-09-23 21:47:40 -0700

    started orm and file structure

---
+++ config/orm.js
@@ -0,0 +1,40 @@
+var connection = require("/connection.js");
+
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
+var orm = {
+    selectAll: function(whatToSelect, tableInput) {
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [whatToSelect, tableInput], function(err, data) {
+            if(err) throw err;
+            console.log(result);
+        });
+    },
+
+    insertOne: function(table, cols, val, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (" + cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(val.length) + ")";
+
+        console.log(queryString);
+
+        connection.query(queryString, val, function(err, result) {
+            if(err) throw err;
+            cb(result);
+        });
+    },
+
+    updateOne: function()
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("double cheeseburger", true), 
+("triple cheeseburger", true), ("animal style burger", true);
